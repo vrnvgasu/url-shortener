f41ef824402cc97f26e8235364c6afd05dc836f4
dmitrii <dmitrii@domain.com>
2024-01-26 23:55:15 +0300

    добавляем конфиги

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,7 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"url-shortener/internal/config"
+)
+
 func main() {
 	// TODO init config: cleanenv - может читать из разных источников
+	cnf := config.MustLoad()
+	fmt.Println(cnf)
 
 	// TODO init logger: slog - в ядре с версии 1.21
 
---
+++ internal/config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+	"os"
+	"time"
+)
+
+// полностью соответсвует структуре config/local.yaml
+type Config struct {
+	// yaml - названия поля в config/local.yaml
+	// env - название параметра из переменной окружения (если читаем от туда)
+	// env-required - приложение не запуститься, если пропустить параметр при установке конфигов
+	Env         string `yaml:"env" env:"ENV" env-default:"local"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+
+// "Must" - сообщаем, что функция может кинуть панику
+func MustLoad() *Config {
+	// берем путь к конфигу из переменной окружения
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+
+	// check is file exist
+	if _, err := os.Stat(configPath); err != nil {
+		log.Fatalf("config file is not exist, %s", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("can't read config, %s", err)
+	}
+
+	return &cfg
+}
