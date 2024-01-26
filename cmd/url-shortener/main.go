a07ccf511c4960b444be48e838884f29dce7d810
dmitrii <dmitrii@domain.com>
2024-01-26 23:22:30 +0300

    план работы

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,5 +1,13 @@
-package url_shortener
+package main
 
 func main() {
+	// TODO init config: cleanenv - может читать из разных источников
 
+	// TODO init logger: slog - в ядре с версии 1.21
+
+	// TODO init storage: sqlite
+
+	// TODO init router: chi, "chi render"
+
+	// TODO run server
 }
