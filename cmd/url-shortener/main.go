d695690c2bc133ed917bb36716479d92e85e441d
dmitrii <dmitrii@domain.com>
2024-01-27 22:49:31 +0300

    добавляем log

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,7 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 	"url-shortener/internal/config"
+)
+
+const (
+	envLocal = "local"
+	endDev   = "dev"
+	endProd  = "prod"
 )
 
 func main() {
@@ -11,10 +19,35 @@ func main() {
 	fmt.Println(cnf)
 
 	// TODO init logger: slog - в ядре с версии 1.21
+	log := setupLogger(cnf.Env)
+	//log = log.With("env", cnf.Env) // добавляем параметр env ко всем логам
+	log.Info("starting application", slog.String("env", cnf.Env))
+	log.Debug("debug messages are enabled")
 
 	// TODO init storage: sqlite
 
 	// TODO init router: chi, "chi render"
 
 	// TODO run server
+}
+
+// лог (вид и уровень) зависит от окружения: dev, prod и тд
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case endDev: // для dev стенда
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case endProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
